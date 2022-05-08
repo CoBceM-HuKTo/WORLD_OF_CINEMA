863994462ea3d90c7b92e35836b4528e95ead782
Akhnina <94619789+Akhnina@users.noreply.github.com>
2022-05-08 22:23:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
 
 
 @app.route('/registration', methods=['GET', 'POST'])
